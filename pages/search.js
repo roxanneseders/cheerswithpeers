6c405bb1c469d4e175762ca0d7a0e4e4e9e39d05
Mitch <francislaxx19@gmail.com>
2018-01-24 14:57:39 -0700

    api ping

---
+++ pages/search.js
@@ -0,0 +1,33 @@
+import Layout from "../comps/MyLayout.js";
+
+("use strict");
+
+const yelp = require("yelp-fusion");
+
+// Place holder for Yelp Fusion's API Key. Grab them
+// from https://www.yelp.com/developers/v3/manage_app
+const apiKey =
+  "Pu9AtNK5eBvInV3K9wYYI61bfEE5zy_NGh9CUI_x02IJEnxb4l26ckygnr20v3IEgoFtkGm68-sao8jPPhhVU1a8NL6l0AGsJrl6VNW_UQM4zDdcKKOeV2IQPmhmWnYx";
+
+const searchRequest = {
+  term: "Bar",
+  location: "phoenix, az"
+};
+
+const client = yelp.client(apiKey);
+
+client
+  .search(searchRequest)
+  .then(response => {
+    const firstResult = response.jsonBody.businesses[4];
+    const prettyJson = JSON.stringify(firstResult, null, 4);
+    console.log(prettyJson);
+  })
+  .catch(e => {
+    console.log(e);
+  });
+export default () => (
+  <Layout>
+    <p>Search here.</p>
+  </Layout>
+);
